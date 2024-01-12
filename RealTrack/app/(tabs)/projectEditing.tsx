21eea3cca2de34c76346d9511ccab6eb9b915447
Striker13337 <57156370+Striker13337@users.noreply.github.com>
2024-01-12 13:52:29 +0100

    Added comments, Visual enhancements
    deleted old useless code,
    aligned the code,
    added comments.

--- RealTrack/app/(tabs)/projectEditing.tsx
+++ RealTrack/app/(tabs)/projectEditing.tsx
@@ -5,91 +5,82 @@ import { Text, View } from '../../components/Themed';
 import { useState } from 'react';
 import { TextInput } from 'react-native-gesture-handler';
 
-function onPressFunction(){
-    console.log("jup gedrückt");
-}
-function onPressGps(){
-    console.log("Gps gedrückt");
-}
-function onPressWlan(){
-    console.log("Wlan gedrückt");
-}
-function sendData(){
+//Functions for pressing the add buttons (placeholders)
 
+function onPressGps() {
+  console.log("Gps pressed");
 }
-function getData(){
-
+function onPressWlan() {
+  console.log("Wlan pressed");
 }
-
-
-
 
 export default function ProjectEditingScreen() {
-    const [name, setName] = useState("test");
-    const [gps, setGps] = useState();
-    const [wlan, setWlan] = useState();
-    const [pause, setPause] = useState();
-    const [timeCount, setTimeCount] = useState();
-    const [goal, setGoal] = useState();
-    
-    
-    return (
-<View style={styles.container}>
-    
-<Text style={styles.text}>Projectname: {name}</Text>
-    <TextInput 
+
+  const [name, setName] = useState("projectname");
+  const [gps, setGps] = useState();
+  const [wlan, setWlan] = useState();
+  const [pause, setPause] = useState("14:00");
+  const [timeCount, setTimeCount] = useState();
+  const [goal, setGoal] = useState("200");
+
+
+  return (
+    //Visualisation of in and outputs for a project
+    <View style={styles.container}>
+
+      <Text style={styles.text}>Projectname: {name}</Text>
+      <TextInput
         style={styles.input}
         placeholder="Name"
-        onChangeText={(val)=> setName(val)}
-    />
+        onChangeText={(val) => setName(val)}
+      />
 
-    <Text style={styles.text}>Gps: {gps}
-    <Button
-    title= "add Gps"
-    onPress={onPressGps}
-    />
-    
-    </Text>
-    <Text style={styles.text}>Wlan : {wlan}
-    <Button
-    title= "add Wlan"
-    onPress={onPressWlan}
-    />
-</Text>
-    <Text style={styles.text}>Breaktime: {pause}</Text>
-    <TextInput 
+      <Text style={styles.text}>Gps: {gps}
+        <Button
+          title="add Gps"
+          onPress={onPressGps}
+        />
+
+      </Text>
+      <Text style={styles.text}>Wlan : {wlan}
+        <Button
+          title="add Wlan"
+          onPress={onPressWlan}
+        />
+      </Text>
+      <Text style={styles.text}>Breaktime: {pause}</Text>
+      <TextInput
         style={styles.input}
-        placeholder="Break time"
-        onChangeText={(val)=> setPause(val)}
-    />
+        placeholder="Breaktime"
+        onChangeText={(val) => setPause(val)}
+      />
 
-    <Text style={styles.text}>Timegoal: {goal} Hours</Text>
-    <TextInput 
+      <Text style={styles.text}>Timegoal: {goal} Hours</Text>
+      <TextInput
+        keyboardType='decimal-pad'
         style={styles.input}
         placeholder="Timegoal"
-        onChangeText={(val)=> setGoal(val)}
-    />
-    <KeyboardAvoidingView  style={styles.addButtonWrapper} >
-      
-      <TouchableOpacity onPress={()=> Vibration.vibrate(100)} >
-        <View style = {styles.addProject}>
-          <Text style = {styles.addText}>+</Text>
-        </View>
-      </TouchableOpacity>
-        
-    </KeyboardAvoidingView>
-</View>
-    );
+        onChangeText={(val) => setGoal(val)}
+      />
+      <KeyboardAvoidingView style={styles.addButtonWrapper} >
+        {/*Using the vibrate function to give feedback on a pressed button*/}
+        <TouchableOpacity onPress={() => Vibration.vibrate(100)} >
+          <View style={styles.addProject}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+
+      </KeyboardAvoidingView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#E8EAED',
     height: '100%',
-    flexDirection:'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    
+    flexDirection: 'column',
+
   },
   title: {
     fontSize: 20,
@@ -100,18 +91,18 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
-  input:{
-    borderWidth:1,
-    borderColor:'grey',
+  input: {
+    borderWidth: 1,
+    borderColor: 'grey',
     padding: 8,
     margin: 10,
   },
-  text:{
-    fontSize : 32,
+  text: {
+    fontSize: 32,
     color: 'black',
 
   },
-  addProject:{
+  addProject: {
     width: 60,
     height: 60,
     backgroundColor: 'white',
@@ -123,13 +114,13 @@ const styles = StyleSheet.create({
 
 
   },
-  addText:{
-    fontSize : 32,
+  addText: {
+    fontSize: 32,
     color: 'black',
   },
-  addButtonWrapper:{
+  addButtonWrapper: {
     position: 'absolute',
-    bottom : 10,
+    bottom: 10,
     right: 50
   }
 
